f37bdc064da9cccd59dbb48c70cf922cc0e6f496
ranjith kumar reddy <ranjithandsridher@gmail.com>
2023-12-27 21:40:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+// Trigger the popup every 5 seconds
+setInterval(openPopup, 10000);
